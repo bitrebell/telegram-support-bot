d1993f41ccab5f56a10f23851b33d2aef8ee605a
Bitrerbell <darkanger008@gmail.com>
2023-09-21 14:52:12 +0530

    hiii

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from telegram.ext import Updater
 
 from handlers import setup_dispatcher
-from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
-
 # Setup bot handlers
 
 TELEGRAM_TOKEN = "6412441114:AAF5nri-Vw1kcwvMn4JT4KzXH2Fjpxv3HHA"
