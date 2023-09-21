50517febaea9a9b9aa9759ecd832e5ecca455717
Bitrerbell <darkanger008@gmail.com>
2023-09-21 14:50:09 +0530

    HII

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from handlers import setup_dispatcher
 from settings import TELEGRAM_TOKEN, HEROKU_APP_NAME, PORT
 
 # Setup bot handlers
+
+TELEGRAM_TOKEN = "6412441114:AAF5nri-Vw1kcwvMn4JT4KzXH2Fjpxv3HHA"
+PORT = '8443'
+
 updater = Updater(TELEGRAM_TOKEN)
 
 dp = updater.dispatcher
